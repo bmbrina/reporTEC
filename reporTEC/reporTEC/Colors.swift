235813db82b6968155ae55de5b6c0c340ac38a1f
Barbara Brina <bmbrina9@gmail.com>
2017-10-19 17:30:47 -0500

    Adds splash screen and login button

---
+++ reporTEC/reporTEC/Colors.swift
@@ -0,0 +1,15 @@
+//
+//  Colors.swift
+//  reporTEC
+//
+//  Created by Barbara Brina on 10/19/17.
+//  Copyright © 2017 Barbara Brina. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    static let mainColor = UIColor(red: 71, green: 107, blue: 172, alpha: 1)
+    
+}
